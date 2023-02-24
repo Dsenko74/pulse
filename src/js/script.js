674f4fd256e81535e812826aa772a43254ea6eb0
Dmytro <dsenko74@gmail.com>
2023-02-24 12:40:30 +0200

    customise mail

--- src/js/script.js
+++ src/js/script.js
@@ -49,6 +49,74 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    //Modal
+    $('[data-modall=consultation]').on('click', function() { //  що беремо — $('[data-modall=consultation]'), що з ним робимо — клікаємо  on('click', function() {, як результат цієї дії береться це — $('.overlay, #consultation'), та потім  це з'являється — fadeIn('slow')
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modall__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    // скіпт який викликаємо, коли натискаємо на кнопу купити
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modall__descr').text($('.catalog-item__subtitle').eq(i).text()); //спершу в $ куди, потім в $ що
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+3 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
 /* var slider = tns({
     container: '.carousel__inner',
